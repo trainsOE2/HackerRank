9dc04555027a1a4a136272f8bc0361d00101b9e8
Megha Singh <meghasingh.ms@gmail.com>
2017-07-06 23:01:19 +0530

    Add files via upload

---
+++ MinFunction.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+int main() {
+    int n;
+    string num;
+    string str;
+    cin >> n;
+        for ( int i=1 ; i<=n ; i++)
+        {
+            if( i==n )
+            {
+                cout << "int";
+            }
+            else
+            {
+                cout << "min(int, " ;
+            }
+
+        }
+        for ( int i=1 ; i<n ; i++)
+            cout << ")";
+
+    return 0;
+}
