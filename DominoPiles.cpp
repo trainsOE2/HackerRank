dbd7392f678213ad18f825b96869da161cfffde2
Megha Singh <meghasingh.ms@gmail.com>
2017-07-21 22:25:13 +0530

    Create DominoPiles.cpp

---
+++ DominoPiles.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+   
+    cout << (m*n)/2;
+   
+    return 0;
+}
