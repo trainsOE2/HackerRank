e17e3fe8f0b1eee5474cee61def0b987520b3e79
Megha Singh <meghasingh.ms@gmail.com>
2017-08-18 20:38:20 +0530

    Update LowerBound-STL.cpp

--- LowerBound-STL.cpp
+++ LowerBound-STL.cpp
@@ -1,1 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n+1);
+    vector<int>::iterator low, it;
+    for( int i=1; i<=n; i++)
+    {
+        cin >> v[i];
+    }
+    int q;
+    int ele;
+    cin >> q;
+    for(int j=1; j<=q; j++)
+    {
+        cin >> ele;
+        low = lower_bound(v.begin(), v.end(), ele);
+        it = find(v.begin(), v.end(), ele);
+        if(it != v.end())
+            cout << "Yes " << low - v.begin() << endl;
+        else
+            cout << "No " << low - v.begin() << endl;
+            
+    }
+    return 0;
+}
 
