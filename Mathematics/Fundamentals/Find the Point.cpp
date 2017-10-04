cfe18c130dbd1373aa6c59e2dd6a30e5c6e699e0
Megha Singh <meghasingh.ms@gmail.com>
2017-10-04 19:32:20 +0530

    Update and rename Find_the_Point to Find the Point.cpp

--- Mathematics/Fundamentals/Find the Point.cpp
+++ Mathematics/Fundamentals/Find the Point.cpp
@@ -1,13 +1,9 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
+
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int n;
     int px, py;
     int qx, qy;
